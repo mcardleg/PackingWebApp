12cfbcf0d4a2662bc7e20958f0dc7a99fcb06f73
Gwen McArdle <mcardleg@tcd.ie>
2021-10-20 15:27:13 +0100

    table info collected

--- server.mjs
+++ server.mjs
@@ -34,6 +34,40 @@ let temp = (list) => {
     return temp
 }
 
+let table_info = (list) => {
+    let temp = []
+    let wind = []
+    let rain = []
+    let table = []
+
+    for (let i=0; i<list.length; i++){
+        temp[i] = list[i].main.temp
+
+        if (list[i].hasOwnProperty('wind')){
+            wind[i] = list[i].wind.speed
+        }
+        else {
+            wind[i] = 0
+        }
+
+        if (list[i].hasOwnProperty('rain')){
+            rain[i] = list[i].rain["3h"]
+        }
+        else {
+            rain[i] = 0
+        }
+    }
+
+    table[0] = temp
+    table[1] = wind
+    table[2] = rain
+    return table
+}
+
+let pollution = (list) => {
+    
+}
+
 let location = "Dublin,Ireland"
 let key = "3e2d927d4f28b456c6bc662f34350957"
 let url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + location + '&appid=' + key
@@ -48,10 +82,14 @@ fetch(url, requestOptions)
     .then(result => {
         let list = []
         list = result.list
-        let umbrella = rain(list)
-        console.log(umbrella)
-        let clothes_type = temp(list)
-        console.log(clothes_type)
+//        let umbrella = rain(list)
+//        console.log(umbrella)
+//        let clothes_type = temp(list)
+//        console.log(clothes_type)
+//        let table = table_info(list)
+//        console.log(table)
+        pollution(list)
+
     })
     .catch(error => console.log('error', error));
 
